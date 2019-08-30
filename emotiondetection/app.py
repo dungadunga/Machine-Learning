8ea45f30701db3a4a331128c23b39624714242da
Go Tae Geon <gtg7784@lab-c.me>
2019-08-30 22:59:35 +0900

    update: emotion - picamera resolution

--- emotiondetection/app.py
+++ emotiondetection/app.py
@@ -12,9 +12,9 @@ DEBUG = True
 
 # instanciate the camera
 camera = PiCamera()
-camera.resolution = (640, 480)
+camera.resolution = (1920, 1080)
 camera.framerate = 30
-rawCapture = PiRGBArray(camera, size=(640, 480))
+rawCapture = PiRGBArray(camera, size=(1920, 1080))
 
 # allow the camera to warmup
 time.sleep(0.1)

3bfcc91535ab38f7b8b00283c119a59ace8f33ed
Go Tae Geon <gtg7784@lab-c.me>
2019-08-30 23:16:49 +0900

    update: emotion detection

--- emotiondetection/app.py
+++ emotiondetection/app.py
@@ -1,7 +1,5 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-from multiprocessing import Process
-from multiprocessing import Queue
 import time
 import cv2
 import numpy as np
@@ -12,9 +10,9 @@ DEBUG = True
 
 # instanciate the camera
 camera = PiCamera()
-camera.resolution = (640, 480)
+camera.resolution = (1920, 1080)
 camera.framerate = 30
-rawCapture = PiRGBArray(camera, size=(640, 480))
+rawCapture = PiRGBArray(camera, size=(1920, 1080))
 
 # allow the camera to warmup
 time.sleep(0.1)
@@ -31,43 +29,9 @@ with tf.gfile.FastGFile("./data/retrained_graph.pb", 'rb') as f:
 # We use the Haar Cascade classifier
 faceDetect = cv2.CascadeClassifier('./data/haarcascade_frontalface_default.xml')
 
-
-def prediction_fn(model, inputQueue, outputQueue, label_lines):
-    while True:
-        if not inputQueue.empty():
-			# grab the frame from the input queue			
-            input_image = inputQueue.get()
-			
-			# make the prediction
-			# feed the detected face (cropped image) to the tf graph
-            predictions = sess.run(model, {'DecodeJpeg:0': input_image})
-            prediction = predictions[0]
-
-            # Get the highest confidence category.
-            prediction = prediction.tolist()
-            max_value = max(prediction)
-            max_index = prediction.index(max_value)
-            predicted_label = label_lines[max_index]
-
-            print("%s (%.2f%%)" % (predicted_label, max_value * 100))
-			# write the detections to the output queue
-            outputQueue.put(predicted_label)
-
 # start the tensorflow session and start streaming and image processing
 sess = tf.Session()
 softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-
-# initialize the input queue (detected faces grayed), output queue (predictions),
-inputQueue = Queue(maxsize=1)
-outputQueue = Queue(maxsize=1)
-predictions = None
-
-# initialize the prediction process
-p = Process(target=prediction_fn, args=(softmax_tensor, inputQueue,
-	outputQueue,label_lines,))
-p.daemon = True
-p.start()
-
 
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -90,13 +54,20 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 
     for (x,y,w,h) in faces:
-        # if our input queue is empty we pile one detected face for prediction
-        if inputQueue.empty():
-            inputQueue.put(gray[y:y+h,x:x+w])
+        #sampleNum = sampleNum+1
+        #cv2.imwrite("./temp_dataset/"+str(sampleNum)+".jpg", gray[y:y+h,x:x+w])
+        
+        # feed the detected face (cropped image) to the tf graph
+        predictions = sess.run(softmax_tensor, {'DecodeJpeg:0': gray[y:y+h,x:x+w]})
+        prediction = predictions[0]
 
-        # if there is a prediction in the output queue, we grab it
-        if not outputQueue.empty():
-            predictions = outputQueue.get()
+        # Get the highest confidence category.
+        prediction = prediction.tolist()
+        max_value = max(prediction)
+        max_index = prediction.index(max_value)
+        predicted_label = label_lines[max_index]
+
+        print("%s (%.2f%%)" % (predicted_label, max_value * 100))
 
         cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
         cv2.waitKey(100)
@@ -110,6 +81,4 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     if key == ord("q"):
         cv2.destroyAllWindows()
         break
-
-
 
